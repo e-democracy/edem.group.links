8b0d50bf08e30af3011749f12f076d6c24cb54c3
Bill Bushey <wbushey@gmail.com>
2013-02-25 14:27:29 -0600

    Initial Commit

---
+++ README.rst
@@ -0,0 +1,17 @@
+Introduction
+===========
+
+The *Links* tab on the *Group* page provides links related to the group. Often,
+these will be links concerning the neighborhood that a group represents, or
+events and references for a commmunity of practice group. This product supplies
+'the change page'_ that allows the contents of the tab to be altered. This
+product especially copies the *Change the About Tab* page defined in
+``gs.group.about``.
+
+The Change Page
+===============
+
+The *Change the Links Tab" page is a simple form that changes the 
+``related_links`` property of the group. This module provides the *Change* page
+itself and the link from the *Properties* list in the *Admin* tab on the
+*Group* page to the *Change* page.
---
+++ setup.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+import os
+from setuptools import setup, find_packages
+from version import get_version
+
+version = get_version()
+
+setup(name='edem.group.links',
+    version=version,
+    description="Provides a WYSIWYG editor for the Links tab of groups",
+    long_description=open("README.txt").read() + "\n" +
+                      open(os.path.join("docs", "HISTORY.txt")).read(),
+    classifiers=[
+      "Development Status :: 1 - Planning",
+      "Environment :: Web Environment",
+      "Framework :: Zope2",
+      "Intended Audience :: Developers",
+      "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+      "Natural Language :: English",
+      "Operating System :: POSIX :: Linux"
+      "Programming Language :: Python",
+      "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+    keywords='',
+    author='Bill Bushey',
+    author_email='bill.bushey@e-democracy.org',
+    url='http://forums.e-democracy.org/',
+    license='other',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['edem', 'edem.group'], 
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'setuptools',
+        'gs.skin.ogn.edem',
+        'gs.content.form',
+        'gs.group.base',
+        'gs.group.home',
+        'gs.viewlet',
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,)
+
+
