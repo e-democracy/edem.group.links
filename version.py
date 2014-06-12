16d8223bb08f6a0e40468c61bddc7b5f4b3f17a0
Bill Bushey <wbushey@gmail.com>
2014-06-12 13:57:13 -0500

    Updated version.py

--- version.py
+++ version.py
@@ -1,9 +1,18 @@
-version='2.00'
-release=False
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
 
-#-------------------------------------------------------------------------------------#
+#-----------------------------------------------------------------------------#
 
-import commands
+version = '2.0.0'
+release = False
+
+#-----------------------------------------------------------------------------#
+
+import sys
+if (sys.version_info < (3, )):
+    from commands import getstatusoutput
+else:
+    from subprocess import getstatusoutput  # lint:ok
 import datetime
 import os
 import glob
@@ -11,30 +20,32 @@ import glob
 class CommandError(Exception):
     pass
 
+
 def execute_command(commandstring):
-    status, output = commands.getstatusoutput(commandstring)
+    status, output = getstatusoutput(commandstring)
     if status != 0:
         raise CommandError
     return output
 
+
 def parse_version_from_package():
     try:
-        pkginfo = os.path.join(glob.glob('*.egg-info')[0],
-                                         'PKG-INFO')
+        pkginfo = os.path.join(glob.glob('*.egg-info')[0], 'PKG-INFO')
     except:
         pkginfo = ''
-    
+
     version_string = ''
     if os.path.exists(pkginfo):
-        for line in file(pkginfo):
+        for line in open(pkginfo):
             if line.find('Version: ') == 0:
                 version_string = line.strip().split('Version: ')[1].strip()
         if not version_string:
             version_string = '%s-dev' % version
     else:
         version_string = version
-    
+
     return version_string
+
 
 def get_version():
     try:
@@ -43,15 +54,17 @@ def get_version():
         dt = datetime.datetime.utcfromtimestamp(float(commitdate))
         datestring = dt.strftime('%Y%m%d%H%M%S')
 
-        version_string = "%s-%s-%s" % (version, datestring, commithash)
+        if release:
+            version_string = version
+        else:
+            version_string = "%s.dev%s-%s" % (version, datestring, commithash)
 
-    except CommandError, IntegerError:
+    except CommandError:
         version_string = parse_version_from_package()
 
     return version_string
 
+
 if __name__ == '__main__':
     print get_version()
-
-
-
+    sys.stdout.write('{0}\n'.format(get_version()))
